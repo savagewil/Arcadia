7700f130f101e5f41954a7a074283b7166037ca4
savagewil <willsvg@gmail.com>
2016-11-22 13:56:12 -0500

    Update PictureHolder.py
    update

--- Components/PictureHolder.py
+++ Components/PictureHolder.py
@@ -6,3 +6,33 @@ Author: William Savage
 Email:savage.programing@gmail.com
 This class displays and handles buttons.
 """
+
+class PictureHolder(Component.Component):
+	def __init__(self, loc, imagePath, **kwargs):
+		self.image = pygame.image.load(imagePath)
+		self.rect = self.image.get_bounding_rect()¶
+		self.location = loc
+		
+		self.
+		self.visible = True
+		
+		if "backgroundColor" in kwargs:
+			self.backColor = pygame.Color(kwargs["backgroundColor"])
+			self.back = True
+		else:
+			self.back = False
+		
+		if "border" in kwargs:
+			self.border = kwargs["border"]
+		elif self.back:
+			self.border = 5
+			
+	def display(self,screen):
+		pass
+		
+	def check(self.mouse):
+		pass
+	
+	
+		
+		
