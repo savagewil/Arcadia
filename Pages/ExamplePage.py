23f4aed69464f177357881d9c76df1684fbcf991
Savagewil <willsvg@gmail.com>
2016-11-22 19:33:05 -0500

    Modified

--- Pages/ExamplePage.py
+++ Pages/ExamplePage.py
@@ -16,5 +16,6 @@ class ExamplePage(Page.Page):
             Components.Label.Label([20, 200], 50, "Some Stuff", ["black", "red"], background=True),
             Components.Button.Button([240, 140], 100, 50, "Back", ["blue", "green"], function=Pages.MainPage.MainPage),
             Components.Button.Button([240, 200], 100, 50, "Exit", ["blue", "green"], function="x"),
-            Components.Hyperlink.Hyperlink([460, 20], 50, "Example", ["red", "red"])
+            Components.Hyperlink.Hyperlink([460, 20], 50, "Example", ["red", "red"]),
+            Components.PictureHolder.PictureHolder([500, 500], "Images/corner.png", backgroundColor="Red")
         ]
