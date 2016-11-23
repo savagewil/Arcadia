123cc5727787ba870c16f9c657e7c65b5c3aff55
Savagewil <willsvg@gmail.com>
2016-11-22 19:50:39 -0500

    Modified

--- Components/Hyperlink.py
+++ Components/Hyperlink.py
@@ -10,16 +10,6 @@ class Hyperlink(Label.Label):
             color2 = self.colors[1]
             color = self.colors[0]
             if self.background:
-                c1 = 255
-                c2 = 255
-                c3 = 255
-                if not color[0] > 235:
-                    c1 = color[0] + 20
-                elif not color[1] > 235:
-                    c2 = color[1] + 20
-                elif not color[2] > 235:
-                    c3 = color[2] + 20
-                color_ = c1, c2, c3, 255
                 rect = pygame.Rect(self.Backloc, self.backLength_Width)
                 pygame.draw.rect(surface, color, rect)
             font = pygame.font.Font(self.font, self.height)
