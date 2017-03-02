7ae08f5a1345a244920a90056418842a59f8de76
Savage <willsvg@gmail.com>
2017-03-01 20:45:54 -0500

    Modified

--- Pages/BayStateBrawlPage.py
+++ Pages/BayStateBrawlPage.py
@@ -4,18 +4,19 @@ import Components
 from Pages import Page
 
 
-class BayStateBrawl(Page.Page):
+class BayStateBrawlPage(Page.Page):
     def __init__(self):
         Page.Page.__init__(self)
         self.backgroundColor = pygame.Color("dark red")
         self.components = [
-            Components.Label.Label([200, 100], 150, "Bay State Brawl", ["red", "black"]),
-            Components.Container.Container(300, 0, 1280, 824, [
+            Components.Label.Label([200, 50], 150, "Bay State Brawl", ["red", "black"]),
+            Components.Container.Container(250, 0, 1280, 924, [
 
-                Components.Button.Button([100, 475], 235, 60, "Main Page", ["black", "white"],
+                Components.Button.Button([1000, 475], 235, 60, "Main Page", ["black", "white"],
                                          textHeight=68,
                                          function=MainPage.MainPage),
 
+                Components.Label.Label([100, 270], 40, "", ["gray", "black"], file="TextDocs/BayStateBrawl.txt")
 
             ], "grey")
         ]
